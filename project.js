1c53eb38eda69160f20ad4da3fdf248285d6e891
hugh thompson <hugh.bill@icloud.com>
2025-07-28 17:13:48 +1200

    trying to get projects page woring

--- project.js
+++ project.js
@@ -1,7 +1,7 @@
 var cards = 10;
 
 var cardsHTML = `
-<div class="project-card">
+<div class="project-card" onclick="openProject()">
 <h3>A Project</h3>
 <p>
     A breif description of the project goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -9,5 +9,37 @@ var cardsHTML = `
 </div>
 `;
 
+async function openProject() {
+    console.log("Opening project...");
+    var new_window = window.open("project_skeleton.html", "childname");
+    console.log("New window opened:", new_window);
+    new_window.postMessage("loadProject", "*");
+    console.log("Message sent to new window to load project");
+}
+
 // Set the HTML for all elements with class 'project-grid'
 var grids = document.getElementsByClassName('project-grid')[0].innerHTML = cardsHTML.repeat(cards);
+
+
+// async function openProject() {
+//     // gets and sets data on the project page
+//     const content = await loadProjectText();
+//     const projectContainer = document.getElementById("content");
+//     if (projectContainer) {
+//         projectContainer.innerHTML = content;
+//     } else {
+//         console.error("Project container not found");
+//     }
+
+    
+// }
+
+// async function loadPreview() {
+//     // Gets and sets data for preview
+// }
+
+// async function loadProjectText() {
+//     const setTimeout = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+//     await setTimeout(1000);
+//     return "Hello this is a project description loaded asynchronously.";
+// }
---
+++ projects/testproj.md
@@ -0,0 +1,7 @@
+---
+title : Test Project
+---
+
+
+# This is a test project
+hello 123, this is a test project lorem ipsum dolro set armor hello hello
