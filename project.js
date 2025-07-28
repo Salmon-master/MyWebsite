e03f4982b10824aa933985ce136f62d6e39283e4
hugh thompson <hugh.bill@icloud.com>
2025-07-28 17:47:11 +1200

    random shit

--- project.js
+++ project.js
@@ -1,6 +1,6 @@
-var cards = 10;
 
-var cardsHTML = `
+
+const cardsHTML = `
 <div class="project-card" onclick="openProject()">
 <h3>A Project</h3>
 <p>
@@ -9,37 +9,19 @@ var cardsHTML = `
 </div>
 `;
 
+class ClassName {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
 async function openProject() {
-    console.log("Opening project...");
-    var new_window = window.open("project_skeleton.html", "childname");
-    console.log("New window opened:", new_window);
-    new_window.postMessage("loadProject", "*");
-    console.log("Message sent to new window to load project");
+    var params = new URLSearchParams();
+    params.append("message", "hello World");
+    var url = "project_skeleton.html?" + params.toString();
+    var new_window = window.open(url, "_self");
+
 }
 
 // Set the HTML for all elements with class 'project-grid'
-var grids = document.getElementsByClassName('project-grid')[0].innerHTML = cardsHTML.repeat(cards);
-
-
-// async function openProject() {
-//     // gets and sets data on the project page
-//     const content = await loadProjectText();
-//     const projectContainer = document.getElementById("content");
-//     if (projectContainer) {
-//         projectContainer.innerHTML = content;
-//     } else {
-//         console.error("Project container not found");
-//     }
-
-    
-// }
-
-// async function loadPreview() {
-//     // Gets and sets data for preview
-// }
-
-// async function loadProjectText() {
-//     const setTimeout = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-//     await setTimeout(1000);
-//     return "Hello this is a project description loaded asynchronously.";
-// }
+// var grids = document.getElementsByClassName('project-grid')[0].innerHTML = cardsHTML.repeat(cards);
